34622fdfc7c1b56c8dae9d925af7a24626020d9d
saudcodes <90344904+saudcodes@users.noreply.github.com>
2022-01-01 01:49:46 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var container = document.getElementById("container");
 let gridNumber = 8;
 
-
+let playerselection = "rainbow";
 
 function grid() {
     let gridArea = gridNumber * gridNumber;
@@ -17,10 +17,21 @@ function grid() {
     let gridPixels = container.querySelectorAll("div");
     gridPixels.forEach(gridPixel => gridPixel.addEventListener("mouseover", function (event) {
         // highlight the mouseover target
-        this.style.backgroundColor = `hsl(${Math.floor(Math.random() * 360)}, 90%, 50%)`
-
+        this.style.backgroundColor = colorGen()
     }));
+
+}
+
+function colorGen() {
+    function randomHsl() {
+        return Math.floor(Math.random() * 360);
+    }
+    let rainbow = `hsl(${randomHsl()}, 100%, 50%)`
+    
+    let gray = `hsl(294, 4%, 41%)`;
+
 }
 
 
 grid() = true;
+
