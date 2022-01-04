d992104f473ca31cefc83e8231eb8d6d290c105d
Saudcodes <saudstunts@gmail.com>
2022-01-04 08:37:34 +0400

    function update

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let value = document.querySelector("#value")
 
 gridRange.addEventListener("mouseup", function() {
     let gridValue = gridRange.value
-    
+    container.innerHTML = "";
     grid(gridValue)
     value.textContent = gridValue;
     
